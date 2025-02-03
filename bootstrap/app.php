d62b60148f64281b9eb32d4862f09b7228e79a99
Mauricio Ferreira <mauriciovictor17@gmail.com>
2025-02-03 13:31:00 -0300

    tarefa-001: chore: retirando rotas de console

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__ . '/../routes/api.php',
         apiPrefix: '',
-        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
